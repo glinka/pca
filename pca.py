13edb1f7531b9994ebc57e12c60aea0800df4da1
tchaikovsky <holiday@princeton.edu>
2014-04-30 15:55:05 -0400

    pca is up and running, really only for three-var data though

--- pca.py
+++ pca.py
@@ -19,29 +19,25 @@ def pca(data, ncomp=None):
     # returning the full transformation
     if ncomp == None:
         ncomp = nvars
-    princ_comps = np.zeros((nvars, ncomp))
-    princ_vars = np.zeros(ncomp)
 
-    for i in range(ncomp):
-        eigvals, eigvects = np.linalg.eig(np.dot(np.transpose(X), X))
-        sorted_indices = np.argsort(eigvals)
-        princ_vars[i] = eigvals[sorted_indices[0]]
-        princ_comp = eigvects[:,sorted_indices[0]]
-        princ_comps[:,i] = princ_comp
-        # need to change shape to take transpose
-        princ_comp.shape = (nvars, 1)
-        X = X - np.dot(np.dot(X, princ_comp), np.transpose(princ_comp))
-    return princ_vars, princ_comps
+    u, s, v = np.linalg.svd(X)
+    # note the "-s" to reverse sorting order
+    sorted_indices = np.argsort(-s)
+    princ_vars = s[sorted_indices[:ncomp]]
+    princ_comps = v[sorted_indices[:ncomp], :]
+    # need to change shape to take transpose
+    return princ_vars, np.transpose(princ_comps)
         
 def test_pca():
     from mpl_toolkits.mplot3d import Axes3D
 
     # define a planar equation
-    z = lambda x, y: 3*x - y
-    npoints = 50
-    noise = np.random.normal(size=npoints)
+    z = lambda x, y: 3*x - y + 4
+    npoints = 100
+    stdev = 0.5
+    noise = stdev*np.random.normal(size=npoints)
     xvals = np.random.uniform(low=-1, high=1, size=npoints)
-    yvals = np.random.uniform(low=-1, high=1, size=npoints)
+    yvals = np.random.uniform(low=-4, high=4, size=npoints)
     zvals = z(xvals, yvals) + noise
 
     data = np.transpose(np.array([xvals, yvals, zvals]))
@@ -51,10 +47,19 @@ def test_pca():
     ncomp = pvar.shape[0]
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    for i in range(ncomp):
-        proj = np.dot(np.dot(data, pcomp[:,:i+1]), np.transpose(pcomp[:,:i+1]))
-        ax.scatter(proj[:,0], proj[:,1], proj[:,2])
-        plt.show()
+    cs = ['b', 'r', 'g']
+    data_avg = np.average(data, 0)
+    centered_data = data - data_avg
+    # do not bother plotting "full" projection, which would give back the original data
+    for i in range(2):
+        # need to add back the averages
+        proj = np.dot(np.dot(centered_data, pcomp[:,:i+1]), np.transpose(pcomp[:,:i+1])) + data_avg
+        ax.scatter(proj[:,0], proj[:,1], proj[:,2], c=cs[i], lw=0)
+        print str(i+1) + 'd projection norm: ', np.linalg.norm(data - proj)
+    # plot original data
+    ax.scatter(data[:,0], data[:,1], data[:,2], c='c', lw=0)
+    plt.show(fig)
+    
     
 
 if __name__ == '__main__':
